9555fed49bac1472f232c1e2680332d0e7010b10
pawpietruszewski <pawpietruszewski@gmail.com>
2021-05-09 17:44:12 +0200

    Move homepage to separate component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,6 @@
-import React from 'react';
 import { GetStaticProps } from 'next';
-import UserTilesGrid from '../src/components/UserTilesGrid';
+import Home from '../src/page/Home';
 import { fetchUsers } from '../src/utility/randomuser';
-
-import { UserTilesGridProps } from '../src/components/UserTilesGrid/UserTilesGrid';
 
 export const getStaticProps: GetStaticProps = async () => {
   try {
@@ -21,14 +18,4 @@ export const getStaticProps: GetStaticProps = async () => {
   }
 };
 
-export default function Home({
-  users,
-}: UserTilesGridProps): JSX.Element {
-  return (
-    <div>
-      <UserTilesGrid
-        users={users}
-      />
-    </div>
-  );
-}
+export default Home;
---
+++ src/page/Home.tsx
@@ -0,0 +1,15 @@
+
+import UserTilesGrid from '../components/UserTilesGrid';
+import { UserTilesGridProps } from '../components/UserTilesGrid/UserTilesGrid';
+
+export default function Home({
+  users,
+}: UserTilesGridProps): JSX.Element {
+  return (
+    <div>
+      <UserTilesGrid
+        users={users}
+      />
+    </div>
+  );
+}
